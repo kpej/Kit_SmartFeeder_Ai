a3ba3a157c2ace4082802885c2d972cd69ea9888
ETboard <106308185+etboard@users.noreply.github.com>
2024-02-27 14:05:28 +0900

    Add files via upload

---
+++ src/PC/Python/start.bat
@@ -0,0 +1,8 @@
+@echo ############################
+@echo start batch program
+@echo 2024.02.27                   
+@echo 한국공학기술연구원
+@echo ############################
+@echo start batch program
+SmartFeeder_Ai.exe
+@pause
